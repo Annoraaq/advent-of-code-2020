c5214056d09ad647cfdd5ad7138a5520db1fa7f1
Johannes Baum <baum@bw-webgestaltung.de>
2020-12-02 08:13:55 +0100

    day 2: refactor

--- day2-1.js
+++ day2-1.js
@@ -1,15 +1,19 @@
 const utils = require('./utils');
 const lines = utils.getInput();
 
-const validPasswords = lines.map(line => {
+function extractValues(line) {
   let [times, match, pw] = line.split(' ');
   const [min, max] = times.split('-').map(e => Number(e));
   match = match.substr(0, match.length - 1);
   return { min, max, match, pw };
-}).filter(({ min, max, match, pw }) => {
+}
+
+function matches({ min, max, match, pw }) {
   const occurrences = pw.split(match).length - 1;
   return occurrences >= min && occurrences <= max;
-});
+}
+
+const validPasswords = lines.map(extractValues).filter(matches);
 
 console.log('number of valid passwords', validPasswords.length);
 
--- day2-2.js
+++ day2-2.js
@@ -1,17 +1,20 @@
 const utils = require('./utils');
 const lines = utils.getInput();
 
-const validPasswords = lines.map(line => {
+function extractValues(line) {
   let [times, match, pw] = line.split(' ');
-  const [firstPos, secondPos] = times.split('-').map(e => Number(e));
+  const [min, max] = times.split('-').map(e => Number(e));
   match = match.substr(0, match.length - 1);
-  return { firstPos, secondPos, match, pw };
-}).filter(({ firstPos, secondPos, match, pw }) => {
-  const isFirstMatch = pw.charAt(firstPos - 1) === match;
-  const isSecondMatch = pw.charAt(secondPos - 1) === match;
+  return { min, max, match, pw };
+}
+
+function matches({ min, max, match, pw }) {
+  const isFirstMatch = pw.charAt(min - 1) === match;
+  const isSecondMatch = pw.charAt(max - 1) === match;
 
   return (isFirstMatch && !isSecondMatch) || (!isFirstMatch && isSecondMatch);
-});
+}
+
+const validPasswords = lines.map(extractValues).filter(matches);
 
 console.log('number of valid passwords', validPasswords.length);
-
