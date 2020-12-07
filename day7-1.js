4809e9a4090dd99cc1486cc7ef8f83e0fa468af7
Johannes Baum <baum@bw-webgestaltung.de>
2020-12-07 10:36:59 +0100

    add day 7

---
+++ day7-1.js
@@ -0,0 +1,42 @@
+const utils = require('./utils');
+const lines = utils.getInput();
+
+const adjList = createBagGraph();
+
+const resultList = new Set();
+dfs(adjList, 'shiny gold', (bag) => {
+  resultList.add(bag);
+});
+
+console.log(resultList.size - 1);
+
+function dfs(adjList, node, visit) {
+  visit(node);
+  for (let neighbour of adjList.get(node)) {
+    dfs(adjList, neighbour, visit);
+  }
+}
+
+function createBagGraph() {
+  const adjacencyList = new Map();
+
+  lines.forEach(line => {
+    const [container, containedRaw] = line.split(' bags contain ');
+    addIfNotSet(adjacencyList, container);
+    const contained = containedRaw.substr(0, containedRaw.length - 1).split(', ');
+    const containedClean = contained.map(containedBags =>
+      containedBags.substr(containedBags.indexOf(' ') + 1).split(' bag')[0]
+    )
+    containedClean.forEach(bag => {
+      addIfNotSet(adjacencyList, bag);
+      adjacencyList.get(bag).add(container);
+    });
+  });
+  return adjacencyList;
+}
+
+function addIfNotSet(adjList, value) {
+  if (!adjList.has(value)) {
+    adjList.set(value, new Set());
+  }
+}
---
+++ day7-2.js
@@ -0,0 +1,55 @@
+const utils = require('./utils');
+const lines = utils.getInput();
+
+const adjList = createBagGraph();
+let resCount = numBags(adjList, ['shiny gold', 0]);
+
+console.log(resCount);
+
+function numBags(adjList, [node, bagCount]) {
+  return [...adjList.get(node)].map(([child, bagCount]) => {
+    return bagCount + bagCount * numBags(adjList, [child, bagCount]);
+  }).reduce(sumReduce, 0);
+}
+
+function createBagGraph() {
+  const adjacencyList = new Map();
+
+  lines.forEach(line => {
+    const [container, contained] = parseLine(line);
+    addIfNotSet(adjacencyList, container);
+
+    contained.forEach(([bagCount, bagType]) => {
+      addIfNotSet(adjacencyList, bagType);
+      adjacencyList.get(container).add([bagType, bagCount]);
+    });
+  });
+  return adjacencyList;
+}
+
+function sumReduce(acc, curr) {
+  return acc + curr;
+}
+
+function parseLine(line) {
+  const [container, containedRaw] = line.split(' bags contain ');
+  const contained = containedRaw.substr(0, containedRaw.length - 1).split(', ');
+  const containedParsed = contained.map(parseBag).filter(validBag);
+  return [container, containedParsed];
+}
+
+function parseBag(bagStr) {
+  const bagCount = Number(bagStr.substr(0, bagStr.indexOf(' ')));
+  const bagType = bagStr.substr(bagStr.indexOf(' ') + 1).split(' bag')[0];
+  return [bagCount, bagType];
+}
+
+function validBag([_bagCount, bagType]) {
+  return bagType !== 'other';
+}
+
+function addIfNotSet(adjList, value) {
+  if (!adjList.has(value)) {
+    adjList.set(value, new Set());
+  }
+}
