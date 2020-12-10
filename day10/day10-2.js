89e5e6c215247a5b27a6ca953e5c4d2aed8fe5a6
Johannes Baum <baum@bw-webgestaltung.de>
2020-12-10 08:30:38 +0100

    refactor

--- day10/day10-2.js
+++ day10/day10-2.js
@@ -1,30 +1,27 @@
 const utils = require('../utils');
-const { findInvalidNo } = require('./day9-utils');
 const adapters = utils.getIntInput();
 
-const dp = new Map();
 adapters.sort((a, b) => a - b);
 
-const joltageSet = new Set(adapters);
-
-
+const dp = new Map();
 dp.set(adapters[adapters.length - 1], 1);
+
+for (let i = adapters.length - 2; i >= 0; i--) {
+  setDpEntry(adapters[i]);
+}
+
+setDpEntry(0);
+
+console.log(dp.get(0));
 
 function getOrZero(map, key) {
   if (!map.has(key)) return 0;
   return map.get(key);
 }
 
-for (let i = adapters.length - 2; i >= 0; i--) {
-  const diff1 = getOrZero(dp, adapters[i] + 1);
-  const diff2 = getOrZero(dp, adapters[i] + 2);
-  const diff3 = getOrZero(dp, adapters[i] + 3);
-  dp.set(adapters[i], diff1 + diff2 + diff3);
+function setDpEntry(index) {
+  const diff1 = getOrZero(dp, index + 1);
+  const diff2 = getOrZero(dp, index + 2);
+  const diff3 = getOrZero(dp, index + 3);
+  dp.set(index, diff1 + diff2 + diff3);
 }
-
-const diff1 = getOrZero(dp, 1);
-const diff2 = getOrZero(dp, 2);
-const diff3 = getOrZero(dp, 3);
-dp.set(0, diff1 + diff2 + diff3);
-
-console.log(dp.get(0));
